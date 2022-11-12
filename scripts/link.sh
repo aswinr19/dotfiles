a663591455fcead906a16981d82a03c775681019
aswin r <aswinr019@gmail.com>
2022-11-12 17:59:41 +0530

    Added link.sh to link dotfiles to desired locations

---
+++ scripts/link.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s ~/Documents/configs/nvim  ~/.config/
+ln -s ~/Documents/configs/rofi  ~/.config/
+ln -s ~/Documents/configs/alacritty  ~/.config/
+ln -s ~/Documents/configs/zsh/.zshrc ~/
+ln -s ~/Documents/configs/i3 ~/.config/
