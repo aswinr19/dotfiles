4c7d1da182484ce4f2b0902c911a857825c43770
aswinr <aswinr019@gmail.com>
2022-12-26 09:56:32 +0000

    Added new entries to install.sh for ubuntu server i3wm setup

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 
-sudo pacman -S alacritty git zsh vlc nvim telegram-desktop chromium htop neofetch
+#sudo pacman -S alacritty git zsh vlc nvim telegram-desktop chromium htop neofetch
+
+
+sudo apt update && sudo apt upgrade && sudo apt install i3 lightdm lightmd-settings slick-greeter xbacklight lxappearence pulseaudio pavucontrol gcc git zsh neovim vlc firefox 
