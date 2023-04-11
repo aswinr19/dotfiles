65bc1929da7259ac265bf4364ad57c065b7bf3b2
aswinr <aswinr019@gmail.com>
2023-04-11 06:32:29 +0530

    added distro check in install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 
-#sudo pacman -S alacritty git zsh vlc nvim telegram-desktop chromium htop neofetch
+if [ -d /etc/pacman.d ]
+  then
+    #if host is based on arch linux run the pacman update command followed by package installs 
+    sudo pacman -Syu
+fi
 
 
-sudo apt update && sudo apt upgrade && sudo apt install i3 lightdm lightmd-settings slick-greeter xbacklight lxappearence pulseaudio pavucontrol gcc git zsh neovim vlc firefox 
+if [ -d /etc/apt ]
+  then
+    #if the host is based on debian or ubuntu based ,
+    #updating mirrors and upgrading packages and installing necessary packages.
+    sudo apt update && 
+    sudo apt upgrade && 
+    sudo apt install i3 lightdm lightmd-settings slick-greeter xbacklight lxappearence pulseaudio pavucontrol gcc git zsh curl vlc firefox 
+
+fi
+#build alacrittty neovim from source
+#copy configs from repo to documents and symlink them.
