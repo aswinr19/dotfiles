16be8cdb16fbe7da6af123fa7576a18b6719738c
aswinr <aswinr019@gmail.com>
2023-11-21 21:27:38 +0530

    Commented out unused symblic links

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ln -s ~/Documents/configs/nvim  ~/.config/
-ln -s ~/Documents/configs/rofi  ~/.config/
+#ln -s ~/Documents/configs/rofi  ~/.config/
 ln -s ~/Documents/configs/alacritty  ~/.config/
 ln -s ~/Documents/configs/zsh/.zshrc ~/
-ln -s ~/Documents/configs/i3 ~/.config/
-ln -s ~/Documents/config/polybar ~/.config/
+#ln -s ~/Documents/configs/i3 ~/.config/
+#ln -s ~/Documents/config/polybar ~/.config/
 
 #root permission needed for running this line
-ln -s ~/Documents/config/X11/xorg.conf /etc/X11
-ln -s ~/Documents/config/X11/xorg.conf.d /etc/X11
+#ln -s ~/Documents/config/X11/xorg.conf /etc/X11
+#ln -s ~/Documents/config/X11/xorg.conf.d /etc/X11
 
 #also make the ploybar launch script executable 
